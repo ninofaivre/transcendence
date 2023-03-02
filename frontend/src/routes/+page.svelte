b06eed979e05f48601c6cb13688136a99a04b873
cassepipe <cassepipe@ymail.com>
2023-03-02 12:51:43 +0100

    Added sveltekit project as frontend

---
+++ frontend/src/routes/+page.svelte
@@ -0,0 +1,4 @@
+<h1> TRANSCENDANCE </h1>
+
+<div><a href="/pong">PLAY</a></div>
+<div><a href="/chat">CHAT</a></div>
---
+++ frontend/src/routes/chat/+page.svelte
@@ -0,0 +1,12 @@
+<h1>Transcendance Chat</h1>
+
+<textarea name="chatbox" id="1" cols={window.innerWidth} rows="2"></textarea>
+
+<style>
+
+textarea {
+    position: fixed;
+    bottom: 1%;
+}
+
+</style>

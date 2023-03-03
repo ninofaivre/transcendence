2e9dbce478856d07427a1018d1448ad0fc590827
cassepipe <cassepipe@ymail.com>
2023-03-03 15:27:08 +0100

    able to add msg to discussion

--- frontend/src/routes/chat/ChatBox.svelte
+++ frontend/src/routes/chat/ChatBox.svelte
@@ -2,14 +2,33 @@
 
 	export let messages: string[] 
 
+	let msg = ""
+
+	function sendMessage() {
+		messages = [...messages, msg]
+		msg = "";
+	}
+
+	function handleKeypress(event: KeyboardEvent) {
+		switch (event.key) {
+			case 'Enter':
+				sendMessage();
+		}
+	}
+
 </script>
 
-<textarea>
-</textarea>
+<!--<textarea bind:value={msg} on:keypress={ handleKeypress }/>-->
+<textarea bind:value={msg} />
+<button on:click={ sendMessage }> Send </button>
 
 <style>
 
 	textarea {
+		position: fixed;
+		bottom: 5%;
+	}
+	button {
 		position: fixed;
 		bottom: 1%;
 	}

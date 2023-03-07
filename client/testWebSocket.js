aaeb4130e85fdbff20470a7cbf7a054ef7c35ba7
nino <nino.faivre@netc.fr>
2023-03-07 17:07:35 +0100

    remove client directory

--- client/testWebSocket.js
+++
@@ -1,5 +0,0 @@
-import  { io } from "https://cdn.socket.io/4.3.2/socket.io.esm.min.js"
-import { getCookie } from "./global.js"
-
-const socket = io("http://88.172.94.204:49153")
-socket.emit("message", "jkldfjklsjkfldj")

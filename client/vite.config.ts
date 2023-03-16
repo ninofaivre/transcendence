8306ce21d2360285dd72e0ee38051caddddc11d4
cassepipe <cassepipe@ymail.com>
2023-03-16 12:43:03 +0100

    Added skeletion client dir

---
+++ client/vite.config.ts
@@ -0,0 +1,15 @@
+import { sveltekit } from '@sveltejs/kit/vite';
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+	plugins: [
+		sveltekit(),
+	 ],
+	build: {
+		minify: false,
+		reportCompressedSize: false,
+	},
+	test: {
+		include: ['src/**/*.{test,spec}.{js,ts}']
+	}
+});

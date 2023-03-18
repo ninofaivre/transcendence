f5671f73cfc578d1f640fc27c3b4af012ff329db
cassepipe <cassepipe@ymail.com>
2023-03-18 19:13:25 +0100

    New deps

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import * as cookieParser from 'cookie-parser'
 import { ValidationPipe } from '@nestjs/common';
+import * as fs from 'fs'
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule, {

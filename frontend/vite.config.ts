75513eacb1bb64d19a0e1b81a7c32ed650bc3e19
cassepipe <cassepipe@ymail.com>
2023-04-08 19:45:20 +0200

    Better vite proxy config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,24 +1,25 @@
-import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vitest/config';
+import { sveltekit } from "@sveltejs/kit/vite"
+import { defineConfig } from "vitest/config"
 
 export default defineConfig({
-    plugins: [
-        sveltekit(),
-    ],
-    build: {
-        minify: false,
-        reportCompressedSize: false,
-    },
-    test: {
-        include: ['src/**/*.{test,spec}.{js,ts}']
-    },
-    server: {
-        https: {
-            key: "../secrets/key.pem",
-            cert: "../secrets/cert.pem",
-        },
-		proxy: {
-			 '/api': 'http://localhost:3000',
+	plugins: [sveltekit()],
+	build: {
+		minify: false,
+		reportCompressedSize: false
+	},
+	test: {
+		include: ["src/**/*.{test,spec}.{js,ts}"]
+	},
+	server: {
+		https: {
+			key: "../secrets/key.pem",
+			cert: "../secrets/cert.pem"
 		},
-    }
-});
+		proxy: {
+			"/api": {
+				target: "http://localhost:3000",
+				changeOrigin: true
+			}
+		}
+	}
+})

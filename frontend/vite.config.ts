d6c39bc256c7d945fb13a10ef34d2a1bbf9dd385
cassepipe <cassepipe@ymail.com>
2023-04-08 19:55:22 +0200

    Even better vite server proxy config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -16,10 +16,25 @@ export default defineConfig({
 			cert: "../secrets/cert.pem"
 		},
 		proxy: {
+			// with options: http://localhost:5173/api/bar-> http://example.com/bar
 			"/api": {
-				target: "http://localhost:3000",
-				changeOrigin: true
+				target: "http://localhost:3000/api",
+				changeOrigin: true,
+				secure: true,
+				configure: (proxy, _options) => {
+					proxy.on("error", (err, _req, _res) => {
+						console.log("proxy error", err)
+					})
+					proxy.on("proxyReq", (_proxyReq, req, _res) => {
+						console.log("Sending Request to the Target:", req.method, req.url)
+					})
+					proxy.on("proxyRes", (proxyRes, req, _res) => {
+						console.log("Received Response from the Target:", proxyRes.statusCode, req.url)
+					})
+				}
 			}
+			// string shorthand: http://localhost:5173/api -> http://localhost:3000/api
+			// "/api": "http://localhost:3000/api",
 		}
 	}
 })

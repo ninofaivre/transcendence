2f6eed333d74ad4fcbe2b41b61cb854cc1a4afd5
cassepipe <cassepipe@ymail.com>
2023-04-10 19:35:54 +0200

    Inited store my_name to 'Anonymousr'

--- frontend/src/lib/stores.ts
+++ frontend/src/lib/stores.ts
@@ -5,16 +5,20 @@ import { fetchGet } from "$lib/global"
 //
 export const logged_in = writable(false)
 
-export const my_name = derived(logged_in, ($logged_in, set) => {
-	if ($logged_in === true) {
-		fetchGet("/api/user/myName")
-			.then((r) => r.json())
-			.then(({ data: name }) => {
-				console.log("Welcome back, " + name)
-				set(name)
-			})
-			.catch((err: any) => {
-				console.error("Failed to fetch user's name", err)
-			})
-	} else set("Anonymous")
-})
+export const my_name = derived(
+	logged_in,
+	($logged_in, set) => {
+		if ($logged_in === true) {
+			fetchGet("/api/user/myName")
+				.then((r) => r.json())
+				.then(({ data: name }) => {
+					console.log("Welcome back, " + name)
+					set(name)
+				})
+				.catch((err: any) => {
+					console.error("Failed to fetch user's name", err)
+				})
+		} else set("Anonymous")
+	},
+	"Anonymous",
+)

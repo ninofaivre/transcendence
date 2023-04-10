d80523dc574e305a8b80e331f9e9753a65cde6e1
cassepipe <cassepipe@ymail.com>
2023-04-10 15:41:07 +0200

    Create a logged_in and and a my_name store

--- frontend/src/lib/stores.ts
+++ frontend/src/lib/stores.ts
@@ -1,3 +1,12 @@
-import { readable } from 'svelte/store'
+import { readable, writable, derived } from "svelte/store"
+import { fetchGet } from "$lib/global"
 
-export const current_user = readable("user");
+// export const logged_in = writable(false)
+//
+export const logged_in = writable(false)
+
+export const my_name = derived(logged_in, async ($logged_in) => {
+	if ($logged_in === true) {
+		return await fetchGet("/api/user/myName")
+	} else return "Anonymous"
+})

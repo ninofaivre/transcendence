0f57abc8afcd05fe65a3c74320cda625a9395fdd
cassepipe <cassepipe@ymail.com>
2023-04-10 17:58:23 +0200

    username in AppBar but only with refresh

--- frontend/src/lib/stores.ts
+++ frontend/src/lib/stores.ts
@@ -5,10 +5,16 @@ import { fetchGet } from "$lib/global"
 //
 export const logged_in = writable(false)
 
-export const my_name = derived(logged_in, async ($logged_in) => {
+export const my_name = derived(logged_in, ($logged_in, set) => {
 	if ($logged_in === true) {
-		let { data: name } = await fetchGet("/api/user/myName").then((r) => r.json())
-		console.log("Welcome back, " + name)
-		return name
-	} else return "Anonymous"
+		fetchGet("/api/user/myName")
+			.then((r) => r.json())
+			.then(({ data: name }) => {
+				console.log("Welcome back, " + name)
+				set(name)
+			})
+			.catch((err: any) => {
+				console.error("Failed to fetch user's name", err)
+			})
+	} else set("Anonymous")
 })

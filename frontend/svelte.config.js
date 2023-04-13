88cf974a39a8b8f7ada7abc3e20463fe7bf543b3
cassepipe <cassepipe@ymail.com>
2023-04-13 17:20:54 +0200

    installed and configured devtool svelte-reactive-preprocessor

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -1,18 +1,19 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/kit/vite';
+import { reactivePreprocess } from 'svelte-reactive-preprocessor';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
-	// for more information about preprocessors
-	preprocess: vitePreprocess(),
+  // Consult https://kit.svelte.dev/docs/integrations#preprocessors
+  // for more information about preprocessors
+  preprocess: [vitePreprocess(), reactivePreprocess()],
 
-	kit: {
-		adapter: adapter( {
-				fallback: 'index.html',
-			}
-		)
-	}
+  kit: {
+    adapter: adapter({
+      fallback: 'index.html',
+    }
+    )
+  }
 };
 
 export default config;

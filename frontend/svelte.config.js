67495fef92fa9e05828d674aaacc6c40ac8c4845
cassepipe <cassepipe@ymail.com>
2023-05-18 20:38:16 +0200

    Proper aliases

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -14,7 +14,9 @@ const config = {
 			fallback: "index.html",
 		}),
 		alias: {
-			"contracts": "../contract",
+			$contracts: "../contract",
+			$types: "lib/types",
+			$stores: "lib/stores",
 		},
 	},
 }

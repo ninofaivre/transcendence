f03ef43d8b195fc81a380d0ac7154985329a1efb
cassepipe <cassepipe@ymail.com>
2023-05-18 10:25:19 +0200

    Added contract alias

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -1,20 +1,22 @@
-import adapter from '@sveltejs/adapter-static';
-import { vitePreprocess } from '@sveltejs/kit/vite';
-import { reactivePreprocess } from 'svelte-reactive-preprocessor';
+import adapter from "@sveltejs/adapter-static"
+import { vitePreprocess } from "@sveltejs/kit/vite"
+import { reactivePreprocess } from "svelte-reactive-preprocessor"
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  // Consult https://kit.svelte.dev/docs/integrations#preprocessors
-  // for more information about preprocessors
-  // preprocess: [vitePreprocess(), reactivePreprocess()],
-  preprocess: [vitePreprocess()],
+	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
+	// for more information about preprocessors
+	// preprocess: [vitePreprocess(), reactivePreprocess()],
+	preprocess: [vitePreprocess()],
 
-  kit: {
-    adapter: adapter({
-      fallback: 'index.html',
-    }
-    )
-  }
-};
+	kit: {
+		adapter: adapter({
+			fallback: "index.html",
+		}),
+		alias: {
+			contracts: "../contract",
+		},
+	},
+}
 
-export default config;
+export default config

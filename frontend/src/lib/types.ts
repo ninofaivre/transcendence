4b3c073b16bd8837d2654e080250b68ff848b927
cassepipe <cassepipe@ymail.com>
2023-05-18 20:59:40 +0200

    With just one import

--- frontend/src/lib/types.ts
+++ frontend/src/lib/types.ts
@@ -1,11 +1,9 @@
 import type { ClientInferResponseBody } from "@ts-rest/core"
 
-import type { chansContract } from "$contracts/chans"
-import type { dmsContract } from "$contracts/dms"
-import type { friendsContract } from "$contracts/friends"
-import type { usersContract } from "$contracts/users"
+import type contract from "$contract"
 
-export type Discussion = ClientInferResponseBody<typeof chansContract.getMyChans, 200>
-export type Chan = Discussion
-export type ChanMessage = ClientInferResponseBody<typeof chansContract.getChanMessages, 200>
-export type DirectMessage = ClientInferResponseBody<typeof dmsContract.getDms, 200>
+export type Chans = ClientInferResponseBody<typeof contract.chans.getMyChans, 200>
+export type ChanMessage = ClientInferResponseBody<typeof contract.chans.getChanMessages, 200>
+
+export type Conversations = ClientInferResponseBody<typeof contract.dms.getDms, 200>
+export type DirectMessage = ClientInferResponseBody<typeof contract.dms.getDms, 200>

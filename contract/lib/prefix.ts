6396afe7beed89d0326709b877c6e82b8d12472d
nino <nino.faivre@netc.fr>
2023-05-26 16:59:17 +0200

    update prefix, event simpler now

--- contract/lib/prefix.ts
+++ contract/lib/prefix.ts
@@ -1,15 +1,13 @@
 import { AppRouter, isAppRoute } from "@ts-rest/core"
+import { join } from "path"
 
 export function prefix<T extends AppRouter>(contract: T, pre: string)
 {
-	pre = pre.replace(/^\//, '').replace(/\/$/, '')
-	if (pre === "")
-		return contract
-	for (const k in contract)
+	pre = `/${pre}`
+	for (const v of Object.values(contract))
 	{
-		let v = contract[k]
 		if (isAppRoute(v))
-			v.path = `/${pre}/${v.path.replace(/^\//, '')}`
+			v.path = join(pre, v.path)
 		else
 			prefix(v, pre)
 	}

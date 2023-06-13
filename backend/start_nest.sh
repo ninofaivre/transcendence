b9c092d0a4f2078fe4b36afeb88deb8ce8aaaf86
nino <nino.faivre@netc.fr>
2023-06-13 13:05:24 +0200

    update some scripts

--- backend/start_nest.sh
+++ backend/start_nest.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-# dirty but tmp
+pnpm exec prisma studio --browser none &
 if [ "$1" != "--watch" ]; then
-	nest build
+    cd ../prisma  || exit 1 && pnpm build
+    cd enums || exit 1 && pnpm build
+    cd ../../contract || exit 1 && pnpm build
+    cd ../backend || exit 1 && nest build && nest start
+else
+    cd ../prisma || exit 1 && pnpm build
+    cd enums || exit 1 && pnpm build --watch &
+    cd ../../contract || exit 1 && pnpm build --watch &
+    cd ../backend || exit 1 && nest start --watch
 fi
-
-pnpm exec prisma studio &
-
-nest start "$@"

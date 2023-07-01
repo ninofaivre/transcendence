a2da598a92a37557613dc6bcee3d24caa5af01de
cassepipe <cassepipe@ymail.com>
2023-07-01 20:56:11 +0200

    Green dot based on online status

--- frontend/src/routes/dms/DiscussionList.svelte
+++ frontend/src/routes/dms/DiscussionList.svelte
@@ -1,6 +1,11 @@
 <script lang="ts">
 	import type { DirectConversation } from "$types"
 	import { sse_store } from "$lib/stores"
+	import { page } from "$app/stores"
+	import { onMount } from "svelte"
+	import { get } from "svelte/store"
+	import { addEventSourceListener } from "$lib/global"
+	import { invalidate } from "$app/navigation"
 
 	export let currentDiscussionId: string
 	export let discussions: DirectConversation[]
@@ -27,6 +32,21 @@
 			console.log("Server message: Update the DM conversation", parsedData)
 		})
 	}
+
+	onMount(() => {
+		const sse = get(sse_store)
+		if (sse) {
+			addEventSourceListener(sse, "CREATED_DM", (data) => {
+				console.log("A new dm was created!")
+				invalidate(":discussions")
+			})
+
+			addEventSourceListener(sse, "UPDATED_DM", (data) => {
+				console.log("a dm was update ???")
+			})
+		} else throw new Error("sse_store is empty ! Grrrr", sse)
+		return () => {}
+	})
 </script>
 
 {#each discussions as d}
@@ -36,7 +56,9 @@
 			class="p-4 font-medium rounded-container-token hover:variant-soft-secondary hover:font-semibold"
 		>
 			{d.otherName}
-			<span class="online-dot text-2xl text-green-700">&#8226</span>
+			{#if d.otherStatus === "ONLINE"}
+				<span class="online-dot text-2xl text-green-700">&#8226</span>
+			{/if}
 		</a>
 	{:else}
 		<a
@@ -44,7 +66,9 @@
 			class="variant-ghost-secondary p-4 font-semibold rounded-container-token"
 		>
 			{d.otherName}
-			<span class="online-dot text-2xl text-green-700">&#8226</span>
+			{#if d.otherStatus === "ONLINE"}
+				<span class="online-dot text-2xl text-green-700">&#8226</span>
+			{/if}
 		</a>
 	{/if}
 {/each}

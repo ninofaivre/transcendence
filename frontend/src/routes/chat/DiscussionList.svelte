dcd18dd573777507263571d1e20459ad20e2531b
cassepipe <cassepipe@ymail.com>
2023-07-02 15:18:59 +0200

    Removed old addEventListeners I had forgotten in chat too

--- frontend/src/routes/chat/DiscussionList.svelte
+++ frontend/src/routes/chat/DiscussionList.svelte
@@ -14,26 +14,13 @@
 				console.log("Up arrow was pressed")
 		}
 	}
-
-	// This should be ok as this route is only accessible to logged in users
-	$: {
-		$sse_store?.addEventListener("CREATED_DM", ({ data }: MessageEvent) => {
-			const parsedData: DirectConversation = JSON.parse(data)
-			console.log("Server message: New DM conversation", parsedData)
-		})
-
-		$sse_store?.addEventListener("UPDATED_DM", ({ data }: MessageEvent) => {
-			const parsedData: DirectConversation = JSON.parse(data)
-			console.log("Server message: Update the DM conversation", parsedData)
-		})
-	}
 </script>
 
 {#each discussions as d}
 	{#if d.id != currentDiscussionId}
 		<a
 			href={`/dms/${d.id}`}
-			class="hover:variant-soft-secondary p-4 font-medium rounded-container-token hover:font-semibold"
+			class="p-4 font-medium rounded-container-token hover:variant-soft-secondary hover:font-semibold"
 		>
 			{d.otherName}
 		</a>

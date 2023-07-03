99f2bd928064b73541a4997c06c326d10b30af67
cassepipe <cassepipe@ymail.com>
2023-07-03 13:09:56 +0200

    Should not give you the option to invite your friend but does so anyways

--- frontend/src/routes/friends/SendFriendRequest.svelte
+++ frontend/src/routes/friends/SendFriendRequest.svelte
@@ -3,30 +3,23 @@
 	import type { AutocompleteOption } from "@skeletonlabs/skeleton"
 
 	import { invitationsClient, usersClient } from "$clients"
-	import { toastStore } from "@skeletonlabs/skeleton"
 	import { invalidate } from "$app/navigation"
 	import { reportUnexpectedCode } from "$lib/global"
+	import { page } from "$app/stores"
 
-	let search_input: string
+	let search_input: string = ""
 	let users: AutocompleteOption[] = []
 
 	async function sendFriendRequest(username: string) {
 		const { status, body } = await invitationsClient.friend.createFriendInvitation({
 			body: { invitedUserName: username },
 		})
-		let message
 		if (status != 201) {
-			message = `Could not create friend request. Server returned code ${status}\n with message \"${
-				(body as any)?.message
-			}\"`
-			console.error(message)
+			reportUnexpectedCode(status, "create friend request", body, "error")
 		} else {
 			invalidate(":friendships")
-			message = "Sent friendship request to " + username
+			console.log("Sent friendship request to " + username)
 		}
-		toastStore.trigger({
-			message,
-		})
 	}
 
 	async function onUserSelection(event: any) {
@@ -34,20 +27,22 @@
 	}
 
 	function getUsernames(input: string) {
-		return usersClient
-			.searchUsers({
-				query: {
-					userNameContains: input,
-				},
-			})
-			.then(({ status, body }) => {
-				console.log(body)
-				if (status === 200) {
-					users = body.map((obj) => {
-						return { label: obj.userName, value: obj.userName }
-					})
-				} else reportUnexpectedCode(status, "create friend request", body)
-			})
+		if (input.length > 2) {
+			return usersClient
+				.searchUsers({
+					query: {
+						userNameContains: input,
+					},
+				})
+				.then(({ status, body }) => {
+					console.log(body)
+					if (status === 200) {
+						users = body.map((obj) => {
+							return { label: obj.userName, value: obj.userName }
+						})
+					} else reportUnexpectedCode(status, "create friend request", body)
+				})
+		}
 	}
 
 	$: getUsernames(search_input)
@@ -56,7 +51,14 @@
 <input class="input" type="search" bind:value={search_input} placeholder="Search user..." />
 
 <div class="card max-h-48 w-full max-w-sm overflow-y-auto p-4" tabindex="-1">
-	<Autocomplete bind:input={search_input} bind:options={users} on:selection={onUserSelection} />
+	{#key $page.data.friendList}
+		<Autocomplete
+			bind:input={search_input}
+			bind:options={users}
+			denylist={$page.data.friendList}
+			on:selection={onUserSelection}
+		/>
+	{/key}
 </div>
 
 <style>

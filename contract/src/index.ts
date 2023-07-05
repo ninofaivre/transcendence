30727a14315a5ee729c2e6079694e1911fd1e93e
cassepipe <cassepipe@ymail.com>
2023-07-05 15:35:55 +0200

    Tidying up

--- contract/src/index.ts
+++ contract/src/index.ts
@@ -1,7 +1,13 @@
-export { contract, SseEvent  } from './contract'
-export { DmEvent, zDmReturn, zDmDiscussionElementReturn } from './routers/dms'
-export { ChanEvent, zCreatePublicChan, zCreatePrivateChan, zChanDiscussionEventReturn, zChanDiscussionElementReturn } from './routers/chans'
-export { zUserProfileReturn } from './routers/users'
-export { zFriendShipReturn } from './routers/friends'
-export { zChanInvitationReturn, zFriendInvitationReturn } from './routers/invitations'
-export { friendsContract } from "./routers/friends"
+export { contract, type SseEvent } from "./contract";
+export { type DmEvent, zDmReturn, zDmDiscussionElementReturn } from "./routers/dms";
+export {
+  type ChanEvent,
+  zCreatePublicChan,
+  zCreatePrivateChan,
+  zChanDiscussionEventReturn,
+  zChanDiscussionElementReturn,
+} from "./routers/chans";
+export { zUserProfileReturn } from "./routers/users";
+export { zFriendShipReturn } from "./routers/friends";
+export { zChanInvitationReturn, zFriendInvitationReturn } from "./routers/invitations";
+export { friendsContract } from "./routers/friends";

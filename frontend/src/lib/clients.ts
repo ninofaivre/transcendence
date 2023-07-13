4c6dd20eb0cc80109300b7b124ed450c686d8933
cassepipe <cassepipe@ymail.com>
2023-07-13 19:18:26 +0200

    Mwahahahahahaha

--- frontend/src/lib/clients.ts
+++ frontend/src/lib/clients.ts
@@ -7,7 +7,7 @@ import { PUBLIC_BACKEND_URL as baseUrl } from "$env/static/public"
 // import * as contract_module from "contract/src/index"
 // const { contract } = contract_module
 
-import { contract } from "contract/src/index"
+import { contract } from "contract"
 
 export const client = initClient(contract, {
 	baseUrl,

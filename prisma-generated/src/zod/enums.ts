eab65e53b7938a48b8316af0d594195de76b3019
nino <nino.faivre@netc.fr>
2023-07-14 22:08:15 +0200

    new patch for @gabrola/zod-prisma zenums with opt in schema.prisma

--- prisma-generated/src/zod/enums.ts
+++ prisma-generated/src/zod/enums.ts
@@ -1,16 +1,5 @@
 import { z } from "zod";
 
-export const zPermissionList = z.enum([
-  "SEND_MESSAGE",
-  "DELETE_MESSAGE",
-  "EDIT",
-  "INVITE",
-  "KICK",
-  "BAN",
-  "MUTE",
-  "DESTROY"
-]);
-
 export const PermissionList = {
   SEND_MESSAGE: 'SEND_MESSAGE',
   DELETE_MESSAGE: 'DELETE_MESSAGE',
@@ -22,25 +11,11 @@ export const PermissionList = {
   DESTROY: 'DESTROY',
 } as const;
 
-export const zRoleApplyingType = z.enum([
-  "NONE",
-  "ROLES",
-  "ROLES_AND_SELF"
-]);
-
 export const RoleApplyingType = {
   NONE: 'NONE',
   ROLES: 'ROLES',
   ROLES_AND_SELF: 'ROLES_AND_SELF',
 } as const;
-
-export const zFriendInvitationStatus = z.enum([
-  "PENDING",
-  "ACCEPTED",
-  "REFUSED",
-  "CANCELED",
-  "BLOCKED_USER"
-]);
 
 export const FriendInvitationStatus = {
   PENDING: 'PENDING',
@@ -49,16 +24,6 @@ export const FriendInvitationStatus = {
   CANCELED: 'CANCELED',
   BLOCKED_USER: 'BLOCKED_USER',
 } as const;
-
-export const zChanInvitationStatus = z.enum([
-  "PENDING",
-  "ACCEPTED",
-  "REFUSED",
-  "CANCELED",
-  "DELETED_CHAN",
-  "BLOCKED_USER",
-  "BANNED_FROM_CHAN"
-]);
 
 export const ChanInvitationStatus = {
   PENDING: 'PENDING',
@@ -70,13 +35,6 @@ export const ChanInvitationStatus = {
   BANNED_FROM_CHAN: 'BANNED_FROM_CHAN',
 } as const;
 
-export const zStatusVisibilityLevel = z.enum([
-  "NO_ONE",
-  "ONLY_FRIEND",
-  "IN_COMMON_CHAN",
-  "ANYONE"
-]);
-
 export const StatusVisibilityLevel = {
   NO_ONE: 'NO_ONE',
   ONLY_FRIEND: 'ONLY_FRIEND',
@@ -84,54 +42,26 @@ export const StatusVisibilityLevel = {
   ANYONE: 'ANYONE',
 } as const;
 
-export const zDmPolicyLevelType = z.enum([
-  "ONLY_FRIEND",
-  "IN_COMMON_CHAN",
-  "ANYONE"
-]);
-
 export const DmPolicyLevelType = {
   ONLY_FRIEND: 'ONLY_FRIEND',
   IN_COMMON_CHAN: 'IN_COMMON_CHAN',
   ANYONE: 'ANYONE',
 } as const;
 
-export const zDirectMessageStatus = z.enum([
-  "ENABLED",
-  "DISABLED"
-]);
-
 export const DirectMessageStatus = {
   ENABLED: 'ENABLED',
   DISABLED: 'DISABLED',
 } as const;
-
-export const zClassicDmEventType = z.enum([
-  "CREATED_FRIENDSHIP",
-  "DELETED_FRIENDSHIP"
-]);
 
 export const ClassicDmEventType = {
   CREATED_FRIENDSHIP: 'CREATED_FRIENDSHIP',
   DELETED_FRIENDSHIP: 'DELETED_FRIENDSHIP',
 } as const;
 
-export const zChanType = z.enum([
-  "PUBLIC",
-  "PRIVATE"
-]);
-
 export const ChanType = {
   PUBLIC: 'PUBLIC',
   PRIVATE: 'PRIVATE',
 } as const;
-
-export const zClassicChanEventType = z.enum([
-  "AUTHOR_LEAVED",
-  "AUTHOR_KICKED_CONCERNED",
-  "AUTHOR_JOINED",
-  "AUTHOR_MUTED_CONCERNED"
-]);
 
 export const ClassicChanEventType = {
   AUTHOR_LEAVED: 'AUTHOR_LEAVED',
@@ -139,3 +69,73 @@ export const ClassicChanEventType = {
   AUTHOR_JOINED: 'AUTHOR_JOINED',
   AUTHOR_MUTED_CONCERNED: 'AUTHOR_MUTED_CONCERNED',
 } as const;
+
+export const zPermissionList = z.enum([
+  "SEND_MESSAGE",
+  "DELETE_MESSAGE",
+  "EDIT",
+  "INVITE",
+  "KICK",
+  "BAN",
+  "MUTE",
+  "DESTROY",
+]);
+
+export const zRoleApplyingType = z.enum([
+  "NONE",
+  "ROLES",
+  "ROLES_AND_SELF",
+]);
+
+export const zFriendInvitationStatus = z.enum([
+  "PENDING",
+  "ACCEPTED",
+  "REFUSED",
+  "CANCELED",
+  "BLOCKED_USER",
+]);
+
+export const zChanInvitationStatus = z.enum([
+  "PENDING",
+  "ACCEPTED",
+  "REFUSED",
+  "CANCELED",
+  "DELETED_CHAN",
+  "BLOCKED_USER",
+  "BANNED_FROM_CHAN",
+]);
+
+export const zStatusVisibilityLevel = z.enum([
+  "NO_ONE",
+  "ONLY_FRIEND",
+  "IN_COMMON_CHAN",
+  "ANYONE",
+]);
+
+export const zDmPolicyLevelType = z.enum([
+  "ONLY_FRIEND",
+  "IN_COMMON_CHAN",
+  "ANYONE",
+]);
+
+export const zDirectMessageStatus = z.enum([
+  "ENABLED",
+  "DISABLED",
+]);
+
+export const zClassicDmEventType = z.enum([
+  "CREATED_FRIENDSHIP",
+  "DELETED_FRIENDSHIP",
+]);
+
+export const zChanType = z.enum([
+  "PUBLIC",
+  "PRIVATE",
+]);
+
+export const zClassicChanEventType = z.enum([
+  "AUTHOR_LEAVED",
+  "AUTHOR_KICKED_CONCERNED",
+  "AUTHOR_JOINED",
+  "AUTHOR_MUTED_CONCERNED",
+]);

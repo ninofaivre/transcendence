58b35fabfe7dc9ec772f3358632bafafd6aca3c7
nino <nino.faivre@netc.fr>
2023-08-10 15:45:09 +0200

    prisma exception filter alpha

---
+++ backend/src/prisma/exception-filter.ts
@@ -0,0 +1,38 @@
+
+import { ExceptionFilter, Catch, ArgumentsHost } from '@nestjs/common';
+import { Request, Response } from 'express';
+import { Prisma } from '@prisma/client'
+import { BaseExceptionFilter } from '@nestjs/core';
+
+@Catch(Prisma.PrismaClientKnownRequestError)
+export class PrismaClientExceptionFilter extends BaseExceptionFilter {
+  catch(exception: Prisma.PrismaClientKnownRequestError, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse<Response>();
+
+    switch (exception.code) {
+        case 'P2002': {
+            response
+                .status(409)
+                .json({
+                    code: "PrismaConflictAlreadyExistingEntity",
+                    message: "Prisma tried to insert an entity that already exist, causing a conflict"
+                })
+            break ;
+        }
+        case 'P2025': {
+            response
+                .status(404)
+                .json({
+                    code: "PrismaNotFoundEntity",
+                    message: "Prisma failed to found an entity"
+                })
+            break ;
+        }
+        default: {
+            super.catch(exception, host)
+            break ;
+        }
+    }
+  }
+}

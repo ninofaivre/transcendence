09a48ecf21e2476bd0add1b233f8bb59f5f682b0
cassepipe <cassepipe@ymail.com>
2023-08-11 22:34:20 +0200

    Button to invite friends to current channel

---
+++ frontend/src/lib/InviteFriendToChan.svelte
@@ -0,0 +1,75 @@
+<script lang="ts">
+	import type { AutocompleteOption } from "@skeletonlabs/skeleton"
+
+	import { Autocomplete, InputChip, toastStore } from "@skeletonlabs/skeleton"
+	import { invalidate } from "$app/navigation"
+	import { client } from "$clients"
+	import { createEventDispatcher } from "svelte"
+
+	export let friendList: string[]
+	export let chan_id: string
+
+	let friendOptions: AutocompleteOption[]
+	$: friendOptions = friendList.map((username) => ({
+		label: username,
+		value: username,
+	}))
+
+	const dispatch = createEventDispatcher()
+
+	let priv: boolean = false
+	let form: HTMLFormElement
+	async function handleDiscussionCreation() {
+		let formdata = new FormData(form) // `form` is bound to the form node
+		const title: string = formdata.get("title") as string
+		const type = priv ? "PRIVATE" : "PUBLIC"
+		const usernames: string[] = formdata.getAll("users") as string[]
+		const { status, body } = await client.chans.createChan({
+			body: { type, title },
+		})
+		console.log("Server returned:", status, body)
+		for (let invitedUserName of usernames) {
+			client.invitations.chan.createChanInvitation({
+				body: { chanId: chan_id, invitedUserName },
+			})
+		}
+		invalidate(":channels") // Seems reasonnable and simpler to reload all the whole channel list
+		dispatch("submit")
+	}
+
+	function validation(_username: string): boolean {
+		return friendList.indexOf(_username) !== -1
+	}
+
+	let input: string
+	let value: string[]
+
+	function onInputChipSelect(event: CustomEvent<AutocompleteOption>) {
+		value = [...value, event.detail.value as string]
+	}
+</script>
+
+<!-- TODO: Try width: min-content; on the form's parent or display: inline-block; on the form element to see if it fixes unwantd widening  -->
+<form bind:this={form} on:submit|preventDefault={handleDiscussionCreation} class="">
+	{#if friendList.length != 0}
+		<label for="invites" class="label">Send invites</label>
+		<InputChip bind:input bind:value name="users" id="invites" {validation} />
+		<Autocomplete
+			bind:input
+			denylist={value}
+			options={friendOptions}
+			on:selection={onInputChipSelect}
+			emptyState="No such friend found"
+			class="card overflow-y-auto p-4"
+		/>
+	{/if}
+	<div class="mt-3">
+		<button type="submit" class="btn variant-filled"> Send Invites </button>
+		<button type="button" class="btn variant-filled" on:click={() => dispatch("cancel")}>
+			Cancel
+		</button>
+	</div>
+</form>
+
+<style>
+</style>

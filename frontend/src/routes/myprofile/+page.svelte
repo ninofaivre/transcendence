c484fa509cf6aac7c710dede7f7ce9cffdae89b3
cassepipe <cassepipe@ymail.com>
2023-08-12 10:49:07 +0200

    Ugly file dropzone

---
+++ frontend/src/routes/myprofile/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import { FileDropzone, FileButton } from "@skeletonlabs/skeleton"
+</script>
+
+<FileDropzone name="pp">
+	<div slot="lead" class="text-3xl">📁</div>
+	<svelte:fragment slot="message">Upload your profile picture</svelte:fragment>
+	<svelte:fragment slot="meta">PNG and JPEG files only</svelte:fragment>
+</FileDropzone>
+
+<style>
+</style>

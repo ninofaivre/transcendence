0c45b00467c1b1321ddfdd82d53014a119d637d3
cassepipe <cassepipe@ymail.com>
2023-08-12 11:13:53 +0200

    form

--- frontend/src/routes/myprofile/+page.svelte
+++ frontend/src/routes/myprofile/+page.svelte
@@ -1,12 +1,18 @@
 <script lang="ts">
 	import { FileDropzone, FileButton } from "@skeletonlabs/skeleton"
+
+	let files: FileList
+
+	function handleSubmit(e: SubmitEvent) {}
 </script>
 
-<FileDropzone name="pp">
-	<div slot="lead" class="text-3xl">📁</div>
-	<svelte:fragment slot="message">Upload your profile picture</svelte:fragment>
-	<svelte:fragment slot="meta">PNG and JPEG files only</svelte:fragment>
-</FileDropzone>
+<form on:submit|preventDefault={handleSubmit}>
+	<FileDropzone name="pp">
+		<div slot="lead" class="text-3xl">📁</div>
+		<svelte:fragment slot="message">Upload your profile picture</svelte:fragment>
+		<svelte:fragment slot="meta">PNG and JPEG files only</svelte:fragment>
+	</FileDropzone>
+</form>
 
 <style>
 </style>

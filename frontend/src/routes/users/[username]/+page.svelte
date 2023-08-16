320e6fa19cbd75783dc7791fafba392d9fc40c51
cassepipe <cassepipe@ymail.com>
2023-08-16 23:38:55 +0200

    user pages first

--- frontend/src/routes/users/[username]/+page.svelte
+++ frontend/src/routes/users/[username]/+page.svelte
@@ -1,5 +1,13 @@
 <script lang="ts">
+	import type { PageData } from "./$types"
+	import { PUBLIC_BACKEND_URL } from "$env/static/public"
+	import { page } from "$app/stores"
+
+	export let data: PageData
 </script>
 
-<style>
-</style>
+<h1>{$page.params.username}</h1>
+<img src="{PUBLIC_BACKEND_URL}/users/{$page.params.username}/profilePicture" alt="profile" />
+
+<h1>{data.username}</h1>
+<img src="{PUBLIC_BACKEND_URL}/users/{data.username}/profilePicture" alt="profile" />
--- frontend/src/routes/users/[username]/+page.ts
+++ frontend/src/routes/users/[username]/+page.ts
@@ -1,6 +1,19 @@
 import type { PageLoad } from "./$types"
 import { client } from "$clients"
 
-export const load: PageLoad = ({ params }) => {
-	return {}
+export const load: PageLoad = async ({ params }) => {
+	const user = await client.users.getUser({
+		params: {
+			userName: params.username,
+		},
+	})
+
+	// const match_history = client.users.getMatchHistory({
+	// 	params: {
+	// 		userName: params.username,
+	// 	},
+	// })
+	// return { user, match_history}
+
+	return { user, username: params.username }
 }

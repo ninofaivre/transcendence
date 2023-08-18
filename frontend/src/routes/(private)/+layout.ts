992417a5c78d233d7716ca1c548d7e03f30249cd
cassepipe <cassepipe@ymail.com>
2023-08-18 15:56:06 +0200

    data.me is available from the layout now

--- frontend/src/routes/(private)/+layout.ts
+++ frontend/src/routes/(private)/+layout.ts
@@ -2,9 +2,17 @@ export const ssr = false
 
 import type { LayoutLoad, LayoutLoadEvent } from "./$types"
 import { client } from "$clients"
+import { checkError } from "$lib/global"
 
 export const load = async ({ depends }: LayoutLoadEvent) => {
-	console.log("layout load function from root layout")
+	console.log("layout load function from root/(private) layout")
+
+	depends(":me")
+	let me: string = "Anonymous"
+	const me_res = await client.users.getMe()
+	if (me_res.status != 200) checkError(me_res, "get current user's name")
+	else me = me_res.body.userName
+
 	depends(":friends")
 	const { status: status2, body: friendships } = await client.friends.getFriends()
 	let friendList: string[] = []
@@ -12,10 +20,10 @@ export const load = async ({ depends }: LayoutLoadEvent) => {
 		friendList = friendships.map((friendship) => friendship.friendName)
 	} else {
 		console.log(
-			`Failed to load friend list. Server returned code ${status2} with message \"${
-				(friendships as any)?.message
-			}\"`,
+			`Failed to load friend list. Server returned code ${status2} with message \"${(
+				friendships as any
+			)?.message}\"`,
 		)
 	}
-	return { friendships, friendList }
+	return { friendships, friendList, me }
 }

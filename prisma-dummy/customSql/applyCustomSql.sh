95dfd1f45cf91aee2f3892f4f2b3ad4335005748
cassepipe <cassepipe@ymail.com>
2023-08-19 22:59:35 +0200

    Reworked prisma-dummy package with dotenv-cli, exit symbolic link

--- prisma-dummy/customSql/applyCustomSql.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-script_location=$(dirname "$0")
-
-source "$script_location/../../.env"
-
-export PGHOST PGUSER PGPORT PGPASSWORD PGDATABASE
-
-psql --file="$script_location/CUSTOM.sql"

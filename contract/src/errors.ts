e2f01b070381219dbc91f5bab4bf710a773a52ec
nino <nino.faivre@netc.fr>
2023-08-20 19:47:39 +0200

    isErrorCode now narrow type

--- contract/src/errors.ts
+++ contract/src/errors.ts
@@ -327,5 +327,6 @@ export function isContractError(toTest: unknown): toTest is ContractError {
 	)
 }
 
-export const isErrorCode = (ret: { status: HTTPStatusCode, body: unknown }, code: Codes) =>
+
+export const isErrorCode = <T extends Codes>(ret: { status: HTTPStatusCode, body: unknown }, code: T): ret is GetError<T> =>
     (isContractError(ret) && ret.body.code === code)

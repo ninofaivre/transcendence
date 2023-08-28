7213560dbf0af25d7fc8459d6cb011bb39b50a2a
cassepipe <cassepipe@ymail.com>
2023-08-28 10:16:12 +0200

    Different types of modal for game invite. What happens on accept ?

--- frontend/src/lib/WaitForGame.svelte
+++ frontend/src/lib/WaitForGame.svelte
@@ -6,16 +6,11 @@
 	let value = timeReplyToInvitation
 	const username = $modalStore[0].meta.username
 
-	game_socket.on("invite", { username }, (res: "accepted" | "refused" | "badRequest") => {
-		if (res === "accepted") {
-			element.innerHTML = "<p>Challenge accepted !</p>"
-		} else {
-			element.innerHTML = "<p>Invitation declined</p>"
-			button.innerText = "Close"
-		}
+	game_socket.emit("invite", { username }, (res: "accepted" | "refused" | "badRequest") => {
+		state = res
 	})
 
-	let i = timeReplyToInvitation // seconds
+	let i = timeReplyToInvitation + 3
 	while (i--) {
 		setTimeout(() => {
 			value -= 1
@@ -29,30 +24,66 @@
 	}
 
 	$: {
-		if (value <= 0) onClose()
+		if (value <= 0) {
+			state = "timedOut"
+		}
 	}
 
-	let element: HTMLDivElement
-	let button: HTMLButtonElement
+	let state: "accepted" | "refused" | "timedOut" | "waiting" | "badRequest" = "waiting"
 </script>
 
 <div class="card grid grid-rows-3 p-8">
-	<p class="">Waiting for challenge to be accepted</p>
-	<div bind:this={element} class="self-center justify-self-center">
-		<ProgressRadial value={(value * 100) / timeReplyToInvitation} width="w-16">
-			{value}
-		</ProgressRadial>
-	</div>
-	<button
-		bind:this={button}
-		on:click={onClose}
-		class="btn btn-sm variant-ghost-error mt-4 h-fit w-fit self-center justify-self-center"
-		>Cancel</button
-	>
+	{#if state === "waiting"}
+		<p class="">Waiting for challenge to be accepted</p>
+		<div class="self-center justify-self-center">
+			<ProgressRadial value={(value * 100) / timeReplyToInvitation} width="w-16">
+				{value}
+			</ProgressRadial>
+		</div>
+		<button
+			on:click={onClose}
+			class="btn btn-sm variant-ghost-error mt-4 h-fit w-fit self-center justify-self-center"
+		>
+			Cancel
+		</button>
+	{:else if state === "accepted"}
+		<p class="">Challenge accepted!</p>
+		<button
+			on:click={onClose}
+			class="btn btn-sm variant-ghost-error mt-4 h-fit w-fit self-center justify-self-center"
+		>
+			Go to game
+		</button>
+	{:else if state === "refused"}
+		<p class="">Invitation declined</p>
+		<button
+			on:click={onClose}
+			class="btn btn-sm variant-ghost-error mt-4 h-fit w-fit self-center justify-self-center"
+		>
+			Close
+		</button>
+	{:else if state === "timedOut"}
+		<p class="">Invitation timed out</p>
+		<button
+			on:click={onClose}
+			class="btn btn-sm variant-ghost-error mt-4 h-fit w-fit self-center justify-self-center"
+		>
+			Close
+		</button>
+	{:else if state === "badRequest"}
+		<p class="">Invitation timed out</p>
+		<button
+			on:click={onClose}
+			class="btn btn-sm variant-ghost-error mt-4 h-fit w-fit self-center justify-self-center"
+		>
+			Close
+		</button>
+	{/if}
 </div>
 
 <style>
-	p {
+	p,
+	button {
 		font-family: "Press Start 2P", "ArcadeClassic", serif;
 	}
 </style>

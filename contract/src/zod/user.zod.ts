474f8a703f87226c22c468ff76502c49f20c856e
cassepipe <cassepipe@ymail.com>
2023-09-06 10:28:10 +0200

    remove @ constraint from zUserName

--- contract/src/zod/user.zod.ts
+++ contract/src/zod/user.zod.ts
@@ -5,6 +5,5 @@ export const zUserName = z
 	.nonempty()
 	.min(3)
 	.max(30)
-    .regex(/^[^@]+$/) // forbid @ char
 export const zUserPassword = z.string().nonempty().min(8).max(150)
 export const zUserStatus = z.enum(["OFFLINE", "ONLINE", "INVISIBLE", "QUEUE", "GAME"])

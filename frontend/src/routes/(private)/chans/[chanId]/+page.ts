45bf73ef229652b3438b1a74e265846b1df37cdd
cassepipe <cassepipe@ymail.com>
2023-09-07 13:17:35 +0200

    asdofjadf

--- frontend/src/routes/(private)/chans/[chanId]/+page.ts
+++ frontend/src/routes/(private)/chans/[chanId]/+page.ts
@@ -3,13 +3,12 @@ import type { PageLoad } from "./$types"
 import { client } from "$lib/clients"
 
 export const load = async ({ depends, params, parent }: PageLoadEvent) => {
-
 	console.log("chans/[chanId]/ page load")
-	depends(`:chans${params.chanId}`)
+	depends(`:chan:${params.chanId}`)
 
-    let { chanList } = await parent()
+	let { chanList } = await parent()
 
-    const chan = chanList.find((el) => el.id === params.chanId)
+	const chan = chanList.find((el) => el.id === params.chanId)
 
 	const { status, body: messages } = await client.chans.getChanElements({
 		params: {
@@ -18,11 +17,11 @@ export const load = async ({ depends, params, parent }: PageLoadEvent) => {
 	})
 	if (status !== 200) {
 		console.log(
-			`Failed to load channel list. Server returned code ${status} with message \"${
-				(messages as any)?.message
-			}\"`,
+			`Failed to load channel list. Server returned code ${status} with message \"${(
+				messages as any
+			)?.message}\"`,
 		)
-        return { messages: [],   }
+		return { messages: [] }
 	}
 	return { messages, chan }
 }

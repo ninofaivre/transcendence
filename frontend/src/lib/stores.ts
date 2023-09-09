8ab84589debb21e4c2160d92c1521eb80afa2c3d
cassepipe <cassepipe@ymail.com>
2023-09-09 15:52:20 +0200

    Created a bunch of stores

--- frontend/src/lib/stores.ts
+++ frontend/src/lib/stores.ts
@@ -7,3 +7,11 @@ export const logged_in = localStorageStore("logged", false)
 
 const num = 0
 export const reload_img = writable(num)
+
+export const dms_store = writable([])
+export const chans_store = writable([])
+export const dm_messages_store = writable([])
+export const chans_messages_store = writable([])
+export const friendships_store = writable([])
+export const friendship_requests_store = writable([])
+export const chan_invitations_store = writable([])
